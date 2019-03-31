b813604212ddce012d909d1e938d4bd294cae52b
natmark <natmark0918@gmail.com>
2019-03-31 22:50:09 +0900

    Fix dummy test

--- Tests/GiftKitTests/GiftKitTests.swift
+++ Tests/GiftKitTests/GiftKitTests.swift
@@ -26,7 +26,7 @@ final class GiftKitTests: XCTestCase {
         let data = pipe.fileHandleForReading.readDataToEndOfFile()
         let output = String(data: data, encoding: .utf8)
 
-        XCTAssertEqual(output, "Hello, world!\n")
+        XCTAssertNotNil(output)
     }
 
     /// Returns path to the built products directory.
