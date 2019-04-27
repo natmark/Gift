d9232b052a51b45321613a7369e89d41d9a78597
natmark <natmark0918@gmail.com>
2019-04-28 02:13:38 +0900

    Refactor Repository struct

--- Sources/GiftKit/Repository.swift
+++ Sources/GiftKit/Repository.swift
@@ -15,7 +15,7 @@ public struct Repository {
     var gitDirectoryURL: URL
     var config: GitConfig?
 
-    init(workTreeURL: URL, checkRepository: Bool = true) throws {
+    private init(workTreeURL: URL, checkRepository: Bool = true) throws {
         let disableAllCheck = !checkRepository
 
         self.workTreeURL = workTreeURL
@@ -38,4 +38,99 @@ public struct Repository {
             throw RepositoryError.unsupportedRepositoryFormatVersion(formatVersion)
         }
     }
+
+    public static func find(with targetURL: URL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)) throws -> Repository {
+        if targetURL.appendingPathComponent(".git").isDirectory {
+            return try Repository(workTreeURL: targetURL)
+        }
+
+        let parentURL = targetURL.deletingLastPathComponent()
+        if parentURL.standardizedFileURL == targetURL.standardizedFileURL {
+            // Bottom case (targetURL is root path)
+            throw RepositoryError.noGitDirectory
+        }
+
+        return try find(with: parentURL)
+    }
+
+    @discardableResult
+    public static func create(with workTreeURL: URL) throws -> Repository {
+        let repository = try Repository(workTreeURL: workTreeURL, checkRepository: false)
+
+        if repository.workTreeURL.isExist {
+            if !repository.workTreeURL.isDirectory {
+                throw RepositoryError.isNotDirectory
+            }
+            if let contents = try? repository.workTreeURL.contents(), !contents.isEmpty {
+                throw RepositoryError.isNotEmpty
+            }
+        } else {
+            try FileManager.default.createDirectory(at: workTreeURL, withIntermediateDirectories: true, attributes: nil)
+        }
+
+        // .git/branches
+        try repository.computeSubDirectoryPathFromPathComponents(["branches"], withMakeDirectory: true)
+        // .git/objects
+        try repository.computeSubDirectoryPathFromPathComponents(["objects"], withMakeDirectory: true)
+        // .git/refs/tags
+        try repository.computeSubDirectoryPathFromPathComponents(["refs", "tags"], withMakeDirectory: true)
+        // .git/refs/heads
+        try repository.computeSubDirectoryPathFromPathComponents(["refs", "heads"], withMakeDirectory: true)
+
+        // .git/description
+        if let descriptionURL = try repository.computeSubFilePathFromPathComponents(["description"]) {
+            let fileObject = "Unnamed repository; edit this file 'description' to name the repository.\n"
+            try fileObject.write(to: descriptionURL, atomically: true, encoding: .utf8)
+        }
+
+        // .git/HEAD
+        if let headURL = try repository.computeSubFilePathFromPathComponents(["HEAD"]) {
+            let fileObject = "ref: refs/heads/master\n"
+            try fileObject.write(to: headURL, atomically: true, encoding: .utf8)
+        }
+
+        // .git/config
+        if let configURL = try repository.computeSubFilePathFromPathComponents(["config"]) {
+            var gitConfig = GitConfig()
+            gitConfig.set(sectionName: "core", key: "repositoryformatversion", value: "0")
+            gitConfig.set(sectionName: "core", key: "filemode", value: "false")
+            gitConfig.set(sectionName: "core", key: "bare", value: "false")
+            try gitConfig.write(to: configURL)
+        }
+
+        return repository
+    }
+
+    @discardableResult
+    private func computeSubFilePathFromPathComponents(_ pathComponents: [String], withMakeDirectory: Bool = false) throws -> URL? {
+
+        if pathComponents.count == 0 {
+            return self.gitDirectoryURL
+        }
+
+        if let _ = try self.computeSubDirectoryPathFromPathComponents(Array(pathComponents.dropLast()), withMakeDirectory: withMakeDirectory) {
+            return self.gitDirectoryURL.appendingPathComponents(pathComponents: pathComponents)
+        }
+        return nil
+    }
+
+    @discardableResult
+    private func computeSubDirectoryPathFromPathComponents(_ pathComponents: [String], withMakeDirectory: Bool = false) throws -> URL? {
+        let path = self.gitDirectoryURL.appendingPathComponents(pathComponents: pathComponents)
+
+        if path.isExist {
+            if path.isDirectory {
+                return path
+            } else {
+                throw RepositoryError.isNotDirectory
+            }
+        }
+
+        if withMakeDirectory {
+            try FileManager.default.createDirectory(at: path, withIntermediateDirectories: true, attributes: nil)
+            return path
+        }
+        return nil
+    }
+    
 }
